7b94d34685423dde1ee8d9f5a9090645a49d1087
Mikko Lehtinen <mikkoleh@office.nbl.fi>
2018-09-25 10:45:08 +0300

    auth server boilerplate

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,10 +1,25 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user){
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next){
+  //user has already had their email and password auth'// auth'd
+  // we just need to give them a token
+  res.send({token: tokenForUser(req.user)});
+}
 
 exports.signup = function(req, res, next){
-
   const email = req.body.email;
   const password = req.body.password;
 
+  if(!email || !password){
+    return res.status(422).send({ error: 'You must provide email and password'})
+  }
   User.findOne({email: email } , function(err, existingUser){
     if(err) { return next(err); }
 
@@ -18,7 +33,7 @@ exports.signup = function(req, res, next){
     });
     user.save(function(err){
       if(err) { return next(err); }
-      res.json(user);
+      res.json({token: tokenForUser(user)});
     });
 
   });
